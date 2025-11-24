6d460f1ee95cf4c2eae34ea4312bbe98fe555da9
diana <dianaboglut02@gmail.com>
2025-11-24 11:25:35 +0200

    first commit

---
+++ src/main/java/com/example/jobassig/JobAssigApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobassig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAssigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobAssigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobassig/JobAssigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobassig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAssigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
